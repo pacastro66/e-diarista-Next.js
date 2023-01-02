0f1f2e15cd06c7beab82f7d5059a325040546e21
pacastro66 <pauloantunes66@hotmail.com>
2023-01-02 09:02:41 -0300

    configurando materialUI

---
+++ src/UI/themes/light-themes.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+
+    palette:{
+        primary:{
+            light: "#9661ff"
+        }
+    }
+})
+
+export default theme;
