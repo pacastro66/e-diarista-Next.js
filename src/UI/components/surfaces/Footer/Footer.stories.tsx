dfa27fd775bfc75b5912aa64a3757a9cb533826d
pacastro66 <pauloantunes66@hotmail.com>
2023-01-07 10:15:08 -0300

    iniciando o footer

---
+++ src/UI/components/surfaces/Footer/Footer.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {ComponentMeta,ComponentStory} from "@storybook/react";
+import Footer from "./Footer"
+
+export default {
+    title: "surfaces/Footer",
+    component: Footer,
+
+} as ComponentMeta<typeof Footer>;
