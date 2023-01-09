67be790a5f6d0e2e0b16cbb98d8007dce26d19c0
pacastro66 <pauloantunes66@hotmail.com>
2023-01-09 09:36:22 -0300

    criando o footer parte 1

--- src/UI/components/surfaces/Footer/Footer.tsx
+++ src/UI/components/surfaces/Footer/Footer.tsx
@@ -1,7 +1,60 @@
-import { FooterContainer } from "./Footer.style";
+import {  Box, List, Typography } from "@mui/material";
+import Link from "UI/components/navigation/Link/Link";
+import { FooterContainer,FooterGrid,FooterListItem,FooterTitle } from "./Footer.style";
+
 
 const Footer = () =>{
-    return <FooterContainer>wwwwwww</FooterContainer>
-}
+    return (          
+           <FooterContainer>
+            <FooterGrid>
+                <div>
+                <FooterTitle> Menu</FooterTitle>
+                <List>
+                    <FooterListItem>
+                        <Link href="encontrar-diarista" 
+                            mui={{color:"inherit", variant:"body2"}}>
+                            Encontre um(a)diarista
+                        </Link>
+                    </FooterListItem>
+
+                    <FooterListItem>
+                        <Link href="encontrar-diarista" 
+                            mui={{color:"inherit", variant:"body2"}}>
+                            Seja um(a)diarista
+                        </Link>
+                    </FooterListItem>
+
+                    <FooterListItem>
+                        <Link href="/" 
+                            mui={{color:"inherit", variant:"body2"}}>
+                           Por que usar o E-diarista?
+                        </Link>
+                    </FooterListItem>
+                    
+                    <FooterListItem>
+                        <Link href="/" 
+                            mui={{color:"inherit", variant:"body2"}}>
+                           Principais Dúvidas
+                        </Link>
+                    </FooterListItem>
+                </List>
+                </div>
+                <Box sx={{maxWidth: "400px"}}>
+                <FooterTitle>Quem Somos</FooterTitle>
+                <Typography variant="body2" sx={{mt:2}}>
+                    E-Diaristas te ajuda a encontrar um profissional perfeito para
+                    realizar a limpeza da sua casa. Garantimos o(a) melhor profissional
+                    com total segurança e praticidade! são milhares de clientes
+                    satisfeitos por todo o país.
+                </Typography>
+                </Box>
+                        
+
+        
+                <FooterTitle>Baixe Nossos aplicativos</FooterTitle>
+            </FooterGrid>
+           </FooterContainer>
+    );
+};
 
 export default Footer;
