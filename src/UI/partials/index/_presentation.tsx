572015a369c73048d4f930234f4682070b7e3bb1
pacastro66 <pauloantunes66@hotmail.com>
2023-02-03 11:34:53 -0300

    Estrutura base da tela de apresentacao

--- src/UI/partials/index/_presentation.tsx
+++ src/UI/partials/index/_presentation.tsx
@@ -1,8 +1,13 @@
-import { SectionContainer } from "./_presentation.styled";
+import { ContainerStyled, SectionContainer } from "./_presentation.styled";
 
 const Presentation =() =>{
     return <SectionContainer>
-        
+                <ContainerStyled>
+                    <div>Titulo</div>
+                    <div>subtitulo</div>
+                    <div>button</div>
+                    <div>imagem</div>
+                </ContainerStyled>
           </SectionContainer>;
 };
 export default Presentation;
